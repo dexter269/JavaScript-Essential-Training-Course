4e671264c7024faca7052848bb19604aabe016d0
Dexter Ellis III <31288510+djellis3@users.noreply.github.com>
2018-07-23 22:00:48 -0400

    Learning the Basics and Working with Data

---
+++ Chapter3/array.js
@@ -0,0 +1,13 @@
+var pens;
+pens = ["red", "blue", "green", "orange"];
+//pens = new Array("red", "blue", "green", "orange"); //short hand
+//var mixedBag = ["keys", 5, false, 568, "one more thing"];
+
+//assign variable to Array
+pens[3] = "purple";
+console.log(pens[3]);
+
+//Grab item from array
+var fourthPen = pens[3];
+
+console.log(fourthPen);
---
+++ Chapter3/nan.js
@@ -0,0 +1,5 @@
+var a = 4;
+var b = "ducks";
+var sum = a - b;
+
+console.log(sum);
